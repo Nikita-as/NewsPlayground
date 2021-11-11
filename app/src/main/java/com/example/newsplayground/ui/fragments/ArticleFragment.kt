c144eebeb33a26071a27e85ca1c6c5d46e6bfcb0
andro <Androiddeveloperik22git>
2021-11-11 21:43:15 +0300

    initial commit

---
+++ app/src/main/java/com/example/newsplayground/ui/fragments/ArticleFragment.kt
@@ -0,0 +1,7 @@
+package com.example.newsplayground.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.newsplayground.R
+
+class ArticleFragment : Fragment(R.layout.fragment_article) {
+}
---
+++ app/src/main/java/com/example/newsplayground/ui/fragments/SavedNewsFragment.kt
@@ -0,0 +1,7 @@
+package com.example.newsplayground.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.newsplayground.R
+
+class SavedNewsFragment: Fragment(R.layout.fragment_saved_news) {
+}
