befa3cbfaea4ef75907fef038cb4bbfe9cafe549
andro <Androiddeveloperik22git>
2021-11-13 23:12:23 +0300

    Internet checking

--- app/src/main/java/com/example/newsplayground/util/constants/Constants.kt
+++ app/src/main/java/com/example/newsplayground/util/constants/Constants.kt
@@ -2,7 +2,7 @@ package com.example.newsplayground.util.constants
 
 class Constants {
     companion object{
-        const val API_KEY = "4c55038e1db04a398324d94b4f43b5f7"
+        const val API_KEY = "youapi"
         const val BASE_URL = "https://newsapi.org"
         const val SEARCH_TIME_DELAY = 500L
         const val QUERY_PAGE_SIZE = 20
